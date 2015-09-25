192c540bc0f88c04ebc2afb6d5adbcdfb47c03f5
Niklas Frykholm <niklas.frykholm@autodesk.com>
2015-09-25 12:12:00 +0200

    Initial commit.

---
+++ plugin/plugin.cpp
@@ -0,0 +1,261 @@
+#include "plugin_foundation/plugin_api.h"
+#include "plugin_foundation/id_string.h"
+#include "plugin_foundation/array.h"
+#include "plugin_foundation/allocator.h"
+#include "plugin_foundation/matrix4x4.h"
+#include "plugin_foundation/vector3.h"
+#include "plugin_foundation/random.h"
+
+#include <string.h>
+
+using namespace PLUGIN_NAMESPACE::stingray_plugin_foundation;
+
+LuaApi *_lua;
+RenderBufferApi *_render_buffer;
+MeshObjectApi *_mesh_api;
+ApplicationApi *_application_api;
+ResourceManagerApi *_resource_api;
+AllocatorApi *_allocator_api;
+AllocatorObject *_allocator_object;
+ApiAllocator _allocator = ApiAllocator(nullptr, nullptr);
+SceneGraphApi *_scene_graph;
+UnitApi *_unit;
+
+struct Vertex
+{
+	Vector3 position;
+	Vector3 normal;
+};
+
+typedef uint16_t Index;
+
+Vertex vertex(float x, float y, float z)
+{
+	Vertex v;
+	v.position = vector3(x, y, z);
+	v.normal = vector3(0, 0, 1);
+	return v;
+}
+
+void draw_aabb(Array<Vertex> &vb, Array<Index> &ib, const Vector3 &min, const Vector3 &max)
+{
+	auto v0 = vb.size();
+
+	vb.push_back(vertex(min.x, min.y, min.z));
+	vb.push_back(vertex(min.x, min.y, max.z));
+	vb.push_back(vertex(min.x, max.y, min.z));
+	vb.push_back(vertex(min.x, max.y, max.z));
+	vb.push_back(vertex(max.x, min.y, min.z));
+	vb.push_back(vertex(max.x, min.y, max.z));
+	vb.push_back(vertex(max.x, max.y, min.z));
+	vb.push_back(vertex(max.x, max.y, max.z));
+
+	Index ids[36] = {0,1,2, 1,3,2, 1,5,3, 5,7,3, 5,4,7, 4,6,7, 4,0,6, 0,2,6, 2,3,6, 3,7,6, 4,5,0, 5,1,0};
+
+	for (auto id : ids)
+		ib.push_back(v0 + id);
+}
+
+struct Building
+{
+	float xmin, xmax;
+	float ymin, ymax;
+	float height;
+};
+
+struct Block {
+	float xmin, xmax;
+	float ymin, ymax;
+	float max_height;
+	int num_buildings;
+	Building *buildings;
+};
+
+Block *make_block(unsigned seed, float xmin, float xmax, float ymin, float ymax)
+{
+	Random r(seed);
+
+	int n = 300;
+	Block &b = *(Block *)_allocator.allocate(sizeof(Block) + n * sizeof(Building));
+	b.xmin = xmin;
+	b.xmax = xmax;
+	b.ymin = ymin;
+	b.ymax = ymax;
+	b.max_height = 100;
+	b.num_buildings = n;
+	b.buildings = (Building *)(&b+1);
+
+	for (int i=0; i<n; ++i) {
+		Building &bu = b.buildings[i];
+		bu.xmin = i*2.0f;
+		bu.ymin = 0.0f;
+		while (bu.xmin > b.xmax)
+		{
+			bu.xmin -= b.xmax;
+			bu.ymin += 3;
+		}
+		bu.xmax = bu.xmin + 1;
+		bu.ymax = bu.ymin + 2;
+		bu.height = r(5.0f, 100.0f);
+	}
+	return &b;
+}
+
+void render_block(Block &b, Array<Vertex> &vb, Array<Index> &ib)
+{
+	// Draw ground
+	draw_aabb(vb, ib, vector3(b.xmin, b.ymin, -1), vector3(b.xmax, b.ymax, 0.05f));
+
+	for (int i=0; i<b.num_buildings; ++i) {
+		const Building &bu = b.buildings[i];
+		draw_aabb(vb, ib, vector3(bu.xmin, bu.ymin, 0), vector3(bu.xmax, bu.ymax, bu.height));
+	}
+}
+
+struct Object {
+	uint32_t vbuffer;
+	uint32_t ibuffer;
+	uint32_t vdecl;
+	uint32_t mesh;
+	Array<Vertex> *vertices;
+	Array<uint16_t> *indices;
+};
+
+static int make_city(struct lua_State *L)
+{
+	auto unit = _lua->getunit(L, 1);
+	auto node_name = _lua->tolstring(L, 2, NULL);
+	auto material = _lua->tolstring(L, 3, NULL);
+
+	const RB_VertexChannel vchannels[] = {
+		{ _render_buffer->format(RB_ComponentType::RB_FLOAT_COMPONENT, true, false, 32, 32, 32, 0), RB_VertexSemantic::RB_POSITION_SEMANTIC, 0, 0, false },
+		{ _render_buffer->format(RB_ComponentType::RB_FLOAT_COMPONENT, true, false, 32, 32, 32, 0), RB_VertexSemantic::RB_NORMAL_SEMANTIC, 0, 0, false },
+	};
+	const uint32_t n_channels = sizeof(vchannels) / sizeof(RB_VertexChannel);
+
+	RB_VertexDescription vdesc;
+	vdesc.n_channels = n_channels;
+	uint32_t stride = 0;
+	for (uint32_t i=0; i!=n_channels; ++i) {
+		stride += _render_buffer->num_bits(vchannels[i].format) / 8;
+		uint32_t n_components = _render_buffer->num_components(vchannels[i].format);
+		vdesc.channels[i] = vchannels[i];
+	}
+
+	Object o;
+	o.vdecl = _render_buffer->create_description(RB_Description::RB_VERTEX_DESCRIPTION, &vdesc);
+
+	RB_VertexBufferView vb_view;
+	vb_view.stride = stride;
+
+	RB_IndexBufferView ib_view;
+	ib_view.stride = 2;
+
+	o.indices = MAKE_NEW(_allocator, Array<uint16_t>, _allocator);
+	o.vertices = MAKE_NEW(_allocator, Array<Vertex>, _allocator);
+
+	Block *block = make_block(0, 0.0f, 100.0f, 0.0f, 100.0f);
+	render_block(*block, *o.vertices, *o.indices);
+
+	o.vbuffer = _render_buffer->create_buffer(o.vertices->size() * vb_view.stride, RB_Validity::RB_VALIDITY_STATIC, RB_View::RB_VERTEX_BUFFER_VIEW, &vb_view, o.vertices->begin());
+	o.ibuffer = _render_buffer->create_buffer(o.indices->size() * ib_view.stride, RB_Validity::RB_VALIDITY_STATIC, RB_View::RB_INDEX_BUFFER_VIEW, &ib_view, o.indices->begin());
+
+	uint32_t node_name_id = IdString64(node_name).id() >> 32;
+	o.mesh = _mesh_api->create(unit, node_name_id, MO_Flags::MO_VIEWPORT_VISIBLE_FLAG | MO_Flags::MO_SHADOW_CASTER_FLAG);
+
+	float bv_min[] = { block->xmin, block->ymin, -1.0f };
+	float bv_max[] = { block->xmax, block->ymax, block->max_height };
+	_mesh_api->set_bounding_box(o.mesh, bv_min, bv_max);
+
+	MO_BatchInfo batch_info = { MO_PrimitiveType::MO_TRIANGLE_LIST, 0, 0, o.indices->size() / 3, 0, 0, 1 };
+	_mesh_api->set_batch_info(o.mesh, 1, &batch_info);
+
+	_mesh_api->add_resource(o.mesh, _render_buffer->lookup_resource(o.vbuffer));
+	_mesh_api->add_resource(o.mesh, _render_buffer->lookup_resource(o.vdecl));
+	_mesh_api->add_resource(o.mesh, _render_buffer->lookup_resource(o.ibuffer));
+
+	void *materials[] = { _resource_api->get("material", material) };
+	_mesh_api->set_materials(o.mesh, 1, materials);
+
+	_lua->pushinteger(L, 0);
+	return 1;
+}
+
+/*
+static int destroy_logo(struct lua_State *L)
+{
+	uint32_t idx = (uint32_t)_lua->tointeger(L, 1);
+	auto &o = (*_objects)[idx];
+
+	o.used = false;
+	_render_buffer->destroy_buffer(o.ibuffer);
+	_render_buffer->destroy_buffer(o.vbuffer);
+	_render_buffer->destroy_description(o.vdecl);
+	_mesh_api->destroy(o.mesh);
+	MAKE_DELETE(_allocator, o.cloth);
+	MAKE_DELETE(_allocator, o.vertices);
+	MAKE_DELETE(_allocator, o.indices);
+
+	return 0;
+}
+*/
+
+static void setup_game(GetApiFunction get_engine_api)
+{
+	_application_api = (ApplicationApi*)get_engine_api(APPLICATION_API_ID);
+	_render_buffer = (RenderBufferApi*)get_engine_api(RENDER_BUFFER_API_ID);
+	_mesh_api = (MeshObjectApi*)get_engine_api(MESH_API_ID);
+	_resource_api = (ResourceManagerApi*)get_engine_api(RESOURCE_MANAGER_API_ID);
+	_unit = (UnitApi*)get_engine_api(UNIT_API_ID);
+	_scene_graph = (SceneGraphApi*)get_engine_api(SCENE_GRAPH_API_ID);
+	_allocator_api = (AllocatorApi*)get_engine_api(ALLOCATOR_API_ID);
+
+	_allocator_object = _allocator_api->make_plugin_allocator("City");
+	_allocator = ApiAllocator(_allocator_api, _allocator_object);
+
+	_lua = (LuaApi*)get_engine_api(LUA_API_ID);
+	_lua->add_module_function("City", "make_city", make_city);
+	// _lua->add_module_function("RenderPlugin", "destroy_logo", destroy_logo);
+
+	//_objects = MAKE_NEW(_allocator, Objects, _allocator);
+	//_free_objects = MAKE_NEW(_allocator, FreeObjects, _allocator);
+}
+
+static void shutdown_game()
+{
+	/*
+	for (auto &o : *_objects) {
+		if (!o.used)
+			continue;
+
+		_render_buffer->destroy_buffer(o.ibuffer);
+		_render_buffer->destroy_buffer(o.vbuffer);
+		_render_buffer->destroy_description(o.vdecl);
+		_mesh_api->destroy(o.mesh);
+		MAKE_DELETE(_allocator, o.cloth);
+		MAKE_DELETE(_allocator, o.vertices);
+		MAKE_DELETE(_allocator, o.indices);
+	}
+	MAKE_DELETE(_allocator, _objects);
+	MAKE_DELETE(_allocator, _free_objects);
+	*/
+	_allocator_api->destroy_plugin_allocator(_allocator_object);
+}
+
+extern "C" {
+
+#ifdef STATIC_LINKING
+	void *get_city_plugin_api(unsigned api)
+#else
+	__declspec(dllexport) void *get_plugin_api(unsigned api)
+#endif
+{
+	if (api == PLUGIN_API_ID) {
+		static struct PluginApi api = {0};
+		api.setup_game = setup_game;
+		api.shutdown_game = shutdown_game;
+		return &api;
+	}
+	return 0;
+}
+}
---
+++ project/boot.lua
@@ -0,0 +1,94 @@
+local Application = stingray.Application
+local World = stingray.World
+local Unit = stingray.Unit
+local ShadingEnvironment = stingray.ShadingEnvironment
+local City = stingray.City
+local Camera = stingray.Camera
+local Mouse = stingray.Mouse
+local Keyboard = stingray.Keyboard
+local Matrix4x4 = stingray.Matrix4x4
+local Vector3 = stingray.Vector3
+local Quaternion = stingray.Quaternion
+
+self = {}
+
+camera = {
+	free_flight_speed = 20.0,
+	rotation_speed = 1.0,
+
+	init = function(self, world, camera_unit)
+		self.world = world
+		self.camera_unit = camera_unit
+	end,
+	update = function(self, dt)
+		local camera = Unit.camera(self.camera_unit, "camera")
+		local q = Camera.local_rotation(camera, self.camera_unit)
+		local p = Camera.local_position(camera, self.camera_unit)
+
+		local input = self:_get_input()
+		q = self:_compute_rotation(q, input, dt)
+		p = self:_compute_translation(p, q, input, dt)
+		local pose = Matrix4x4.from_quaternion(q)
+		Matrix4x4.set_translation(pose, p)
+		Camera.set_local_pose(camera, self.camera_unit, pose)
+	end,
+
+	_get_input = function(self)
+		local input = {}
+		input.pan = Mouse.axis(Mouse.axis_id("mouse"))
+		input.move = Vector3 (
+			Keyboard.button(Keyboard.button_id("d")) - Keyboard.button(Keyboard.button_id("a")),
+			Keyboard.button(Keyboard.button_id("w")) - Keyboard.button(Keyboard.button_id("s")),
+			0
+		)
+		return input
+	end,
+	_compute_rotation = function(self, qo, input, dt)
+		local cm = Matrix4x4.from_quaternion(qo)
+
+		local q1 = Quaternion( Vector3(0,0,1), -Vector3.x(input.pan) * self.rotation_speed * dt )
+		local q2 = Quaternion( Matrix4x4.x(cm), -Vector3.y(input.pan) * self.rotation_speed * dt )
+		local q = Quaternion.multiply(q1, q2)
+		local qres = Quaternion.multiply(q, qo)
+		return qres
+	end,
+	_compute_translation = function(self, p, q, input, dt)
+		local pose = Matrix4x4.from_quaternion(q)
+		local local_move = input.move * dt * self.free_flight_speed
+		local move = Matrix4x4.transform(pose, local_move)
+		return p + move
+	end
+}
+
+function init()
+	self.world = Application.new_world()
+	self.viewport = Application.create_viewport(self.world, "default")
+	self.shading_environment = World.create_shading_environment(self.world, "core/stingray_renderer/environments/midday/midday")
+	self.camera_unit = World.spawn_unit(self.world, "core/units/camera")
+	self.sky = World.spawn_unit(self.world, "core/editor_slave/units/skydome/skydome")
+	self.city_unit = World.spawn_unit(self.world, "core/units/camera")
+	City.make_city(self.city_unit, "rp_root", "city")
+
+	camera:init(self.world, self.camera_unit)
+end
+
+function update(dt)
+	camera:update(dt)
+	self.world:update(dt)
+	if Keyboard.button(Keyboard.button_id("esc")) > 0 then
+		Application.quit()
+	end
+end
+
+function render()
+	ShadingEnvironment.blend(self.shading_environment, {"default", 1})
+	ShadingEnvironment.apply(self.shading_environment)
+	local camera = Unit.camera(self.camera_unit, "camera")
+	Application.render_world(self.world, camera, self.viewport, self.shading_environment)
+end
+
+function shutdown()
+	Application.destroy_viewport(self.world, self.viewport)
+	World.destroy_shading_environment(self.world, self.shading_environment)
+	Application.release_world(self.world)
+end
